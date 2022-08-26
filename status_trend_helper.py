7b59d42b90b6df00a91c87aeb4ddd3a9c1ff684f
Jonas Kirk Pedersen <Jonas_pedersen@hotmail.com>
2022-08-26 13:22:06 +0200

    Updated code and description

--- status_trend_helper.py
+++ status_trend_helper.py
@@ -1,18 +1,20 @@
-import pandas as pd
+import pandas as pd 
 import datetime as datetime
+import numpy as np
 import logging
 import pymannkendall as mk
 
-
 class TrendThreshold(object):
   """
-  Trend Threshold object for trend calculation.
+  Trend Threshold object for trend calculation. 'timestamp_of_status' and 'value' are to custom collumn names. 
+  These need to be change to your table collumn names
   :param avg_period: An average period in datapoints to calculated a moving avg. for Trending data is in- or decreasing
   :param avg_period_short: A more agile mva for detecting spikes in data
   :param threshold: Deviation that a spike may have in percentage
   :param runSchedulesBack: Scheduled datapoint to go back to detect spikes in data
   :param debug: Default false. Set to true if testing locally
   """
+
   def __init__(self, avg_period=7, avg_period_short=3, threshold=10, runSchedulesBack=15, debug=False):
     self.avg_period = avg_period
     self.avg_period_short = avg_period_short
@@ -24,7 +26,7 @@ class TrendThreshold(object):
     """
     Analyse dataframe of table history data to form mva and upper/lower limits
     :param df:
-    :return df, upper_threshold, lower_threshold, moving_averages, moving_averages_short, result.trend:
+    :return upper_threshold, lower_threshold, moving_averages, moving_averages_short, result.trend:
     """
     i = 0
     moving_averages = []
@@ -42,7 +44,6 @@ class TrendThreshold(object):
         moving_averages_short.append(window_average_short)
         upper_threshold.append(window_average_short + (window_average_short * self.threshold) / 100)
         lower_threshold.append(window_average_short - (window_average_short * self.threshold) / 100)
-
 
         i = i + 1
 
@@ -69,7 +70,7 @@ class TrendThreshold(object):
       trend_status = df["value"][-(self.avg_period_short + self.avg_period):].values.tolist()
       result = mk.original_test(trend_status)
 
-      return df, upper_threshold, lower_threshold, moving_averages, moving_averages_short, result.trend
+      return upper_threshold, lower_threshold, moving_averages, moving_averages_short, result.trend
     except IndexError:
       if self.debug:
         print("Not enough datapoint to calculate trend. Adjust datapoints or let system create the missing datapoints")
@@ -78,48 +79,34 @@ class TrendThreshold(object):
         logging.info(
           "Not enough datapoint to calculate trend. Adjust datapoints or let system create the missing datapoints")
 
-  def setGrafData(self, df, upper, lower):
-    """
-
-    :param df:
-    :param upper:
-    :param lower:
-    :return graphDataValuePoints:
-    """
-    graphDataValuePoints = []
-    for i in range(0,len(df)):
-      graphDataValuePoints.append([
-      df["timestamp_of_status"][i],
-      df["value"][i],
-      upper[i],
-      lower[i]
-      ])
-    return graphDataValuePoints
-
   def spike_detection(self, dataframe=None):
     """
     Detect spike from analysed data and log if spike has been detected.
     :param dataframe:
-    :return deltavalue: showing a +- value for how high or low the data value is from limits
+    :return returningDataframe, spikeValue, trend: Three variables are returned, 1st is a dataframe containing time, value and thresholds. 2nd is the diviation from the trend and the last is the status of the trend
     """
     timeValue = []
     thresholdIndex = 0
     spikeValue = 0
     try:
       df = pd.DataFrame(dataframe)
-      df = self.analyse_data(df)
+      (upper_threshold, lower_threshold, moving_averages, moving_averages_short, trend) = self.analyse_data(df)
+      returningDataframe = df
+      returningDataframe["timestamp_of_status"] = df["timestamp_of_status"].astype(np.int64)
+      returningDataframe["upperThreshold"] = pd.Series(upper_threshold)
+      returningDataframe["lowerThreshold"] = pd.Series(lower_threshold)
 
-      data = df[0][-self.runSchedulesBack:]
-      upperThreshold = df[1][-self.runSchedulesBack:]
-      lowerThreshold = df[2][-self.runSchedulesBack:]
+      data = df[-self.runSchedulesBack:]
+      upperThreshold = upper_threshold[-self.runSchedulesBack:]
+      lowerThreshold = lower_threshold[-self.runSchedulesBack:]
       spike = False
       for time, value in zip(data["timestamp_of_status"], data["value"]):
         if value > upperThreshold[thresholdIndex]:
           spike = True
-          timeValue = [time, value, upperThreshold[thresholdIndex], df[5], "high increase in data"]
+          timeValue = [time, value, upperThreshold[thresholdIndex], trend, "high increase in data"]
         if value < lowerThreshold[thresholdIndex]:
           spike = True
-          timeValue = [time, value, lowerThreshold[thresholdIndex], df[5], "possible missing data"]
+          timeValue = [time, value, lowerThreshold[thresholdIndex], trend, "possible missing data"]
         thresholdIndex += 1
       if spike:
         if self.debug:
@@ -131,8 +118,6 @@ class TrendThreshold(object):
             f"Spike detected - Trend: {timeValue[3]} - but {timeValue[2]} at {datetime.datetime.strptime(str(timeValue[0]).split('.')[0], '%Y-%m-%d %H:%M:%S')} with value: {timeValue[1]}")
           spikeValue = round(timeValue[1] - timeValue[2])
 
-      graphValueData = self.setGrafData(df=df[0],upper=df[1], lower=df[2])
-
-      return graphValueData, spikeValue, df[5]
+      return returningDataframe, spikeValue, trend
     except:
       logging.info("Error in calculating trend for table")
